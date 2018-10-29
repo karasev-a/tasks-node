e5a6333816e5b4775221037d969df66ae3e25cf9
Yegor Dyomin <2048tb@gmail.com>
2018-10-29 08:37:09 +0200

    - fix token parsing from client - adding payload using

--- lib/middleware/isLogin.ts
+++ lib/middleware/isLogin.ts
@@ -2,13 +2,15 @@ import UserService from "../models/user/service/user.service";
 import { NextFunction, Request, Response } from "express";
 
 export const isLogin = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers["x-access-token"]; // #TODO: need parce
+    const token = req.headers["authorization"].replace(/Bearer /, ''); // remove Bearer from token
     if (!token) {
         return res.status(401).send({ auth: false, message: "No token provided." });
     }
 
-    if (UserService.isAuth(token)) {
+    const payload = UserService.isAuth(token);
+    if (payload) {
         // res.status(200).send({ auth: true, message: "authenticate token." });
+        req["roleId"] = payload.roleId;
         next();
     } else {
         res.status(401).send({ auth: true, message: "wrong token." });
