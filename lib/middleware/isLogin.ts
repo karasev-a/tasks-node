f8332e9e97d130e4100ad751b1cf7a0b318d1759
Yegor Dyomin <yegor.dyomin@techs.com.ua>
2018-10-29 14:47:34 +0200

    remove Bearer

--- lib/middleware/isLogin.ts
+++ lib/middleware/isLogin.ts
@@ -2,15 +2,15 @@ import UserService from "../models/user/service/user.service";
 import { NextFunction, Request, Response } from "express";
 
 export const isLogin = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers["authorization"].replace(/Bearer /, ''); // remove Bearer from token
+    const token = req.headers["authorization"];
     if (!token) {
         return res.status(401).send({ auth: false, message: "No token provided." });
     }
 
     const payload = UserService.isAuth(token);
     if (payload) {
-        // res.status(200).send({ auth: true, message: "authenticate token." });
         req["roleId"] = payload.roleId;
+        req["userId"] = payload.userId;
         next();
     } else {
         res.status(401).send({ auth: true, message: "wrong token." });
