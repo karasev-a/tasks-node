2a983217f6da3e6ddbfbed99d92ecd2c3a6d45c2
Yegor Dyomin <yegor.dyomin@techs.com.ua>
2018-11-15 14:33:13 +0200

    small fixes

--- lib/server/models/express-app.ts
+++ lib/server/models/express-app.ts
@@ -35,13 +35,12 @@ class App {
       res.setHeader("Access-Control-Allow-Headers",
       "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, "
       + "Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Cache-Control, Authorization");
-      // res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+      res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
       if (req.method === "OPTIONS") {
         res.status(204).end();
         return;
       } else {
         res.setHeader("Access-Control-Allow-Credentials", "true");
-        res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
         next();
       }
     });
