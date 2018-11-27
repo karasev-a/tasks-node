a074b7de9b3037d9d97ab2cab7e202b34894fd2d
karasev-a <karasev.avk@gmail.com>
2018-11-27 19:10:06 +0200

    start socket

--- server.ts
+++ server.ts
@@ -2,20 +2,31 @@
 import * as http from "http";
 import app from "./lib/server/models/express-app";
 import DBService from "./lib/db/services/db-service";
-
-import loggers from "./lib/tools/loggers";
+import * as socketIo from "socket.io";
 
 const initApp = async () => {
   try {
     await DBService.initDataBase();
     const port = app.get("port");
     const server = http.createServer(app);
+    const io = socketIo(server);
+    io.of("/socket").on("connection", (socket) => {
+      console.log("user connected");
+
+      socket.on("disconnect", () => {
+        console.log("user disconnected");
+      });
+
+      socket.on("add-message", (message) => {
+        io.emit("message", { type: "new-message", text: message });
+      });
+    });
     server.listen(port, () => {
       global.logger.info(`Server started on port ${port}`);
     });
   } catch (error) {
-   // logger.info(error);
-   global.logger.error(error);
+    // logger.info(error);
+    global.logger.error(error);
   }
 };
 
